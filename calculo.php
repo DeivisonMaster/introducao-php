a6fceec04588061d49a9f60c257d0946ef7cfafd
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-08-20 16:03:41 -0400

    validando valores numéricos

--- calculo.php
+++ calculo.php
@@ -10,13 +10,19 @@ if($_POST){ // validação caso não seja uma submissão via POST
     $valorDiesel    = 3.90;
 
     if(is_numeric($distancia) && is_numeric($autonomia)){ // validação caso dados não sejam numericos
-        $calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
-        $calculoAlcool   = ($distancia / $autonomia) * $valorAlcool;
-        $calculoDiesel   = ($distancia / $autonomia) * $valorDiesel;
+        
+        if($distancia > 0 && $autonomia > 0){
+            $calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
+            $calculoAlcool   = ($distancia / $autonomia) * $valorAlcool;
+            $calculoDiesel   = ($distancia / $autonomia) * $valorDiesel;
 
-        print "<p>Valor Gasolina R$ ".$calculoGasolina."</p>";
-        print "<p>Valor Gasolina R$ ".$calculoAlcool."</p>";
-        print "<p>Valor Gasolina R$ ".$calculoDiesel."</p>";
+            print "<p>Valor Gasolina R$ ".$calculoGasolina."</p>";
+            print "<p>Valor Gasolina R$ ".$calculoAlcool."</p>";
+            print "<p>Valor Gasolina R$ ".$calculoDiesel."</p>";
+        }else{
+            print "<p>O valor recebido não é número válido</p>";    
+        }
+        
     }else{
         print "<p>O valor recebido não é numérico</p>";
     }
