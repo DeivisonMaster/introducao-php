04aa9897288c566cc0c6557ab628bd70805c790f
DeivisonMaster <deivisonoliveira.info@gmail.com>
2019-08-20 15:45:04 -0400

    realizando calculo do consumo

--- calculo.php
+++ calculo.php
@@ -1,8 +1,21 @@
 <?php
 
-$distancia = $_POST["distancia"];
-$autonomia = $_POST["autonomia"];
+    $distancia = $_POST["distancia"];
+    $autonomia = $_POST["autonomia"];
 
-print $distancia;
+    $valorGasolina  = 4.80;
+    $valorAlcool    = 3.80;
+    $valorDiesel    = 3.90;
+
+    $calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
+    $calculoAlcool   = ($distancia / $autonomia) * $valorAlcool;
+    $calculoDiesel   = ($distancia / $autonomia) * $valorDiesel;
+
+    print "<p>Valor Gasolina R$ ".$calculoGasolina."</p>";
+    print "<p>Valor Gasolina R$ ".$calculoAlcool."</p>";
+    print "<p>Valor Gasolina R$ ".$calculoDiesel."</p>";
+
+
+
 
 ?>
