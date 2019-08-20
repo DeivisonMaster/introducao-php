4ec25a4f363208eb549d032293ea86c07cd62958
Deivison de Oliveira Matos <deivison.matos@irede.net>
2019-08-20 15:05:48 -0400

    primeiro script PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width; initial-scale=1"/>
+        <title>Calculo de Consumo de combustivel</title>
+    </head>
+</html>
+<body>
+
+<?php
+    print '<h1>Hello World</h1>';
+?>
+
+</body>
+</html>
+
